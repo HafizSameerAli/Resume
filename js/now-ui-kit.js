1e98d079835829c6b997ccb60b2ae246a19cd045
HafizSameerAli <66215150+HafizSameerAli@users.noreply.github.com>
2021-02-15 14:52:42 +0500

    update index

--- js/now-ui-kit.js
+++ js/now-ui-kit.js
@@ -16,6 +16,31 @@
 
  */
 
+
+document.addEventListener('contextmenu', event=> event.preventDefault()); 
+document.onkeydown = function(e) { 
+if(event.keyCode == 123) { 
+return false; 
+} 
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){ 
+return false; 
+} 
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){ 
+return false; 
+} 
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){ 
+return false; 
+} 
+} 
+  
+ document.addEventListener('contextmenu', function(e) {
+   e.preventDefault();
+ });
+
+ window.oncontextmenu = null;
+
+ 
+
 var transparent = true;
 
 var transparentDemo = true;
